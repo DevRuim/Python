cfe640dc9f08576fd64d2326e7e86912337f5eae
DevRuim <164084580+DevRuim@users.noreply.github.com>
2024-10-04 22:59:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# Crie uma lista vazia chamada numeros
+
+numeros = []
+
+# Adicione os números 5, 10, 15 à lista usando o método append()
+
+numeros.append(5)
+
+numeros.append(10)
+
+numeros.append(15)
+
+# Adicione o número 20 na segunda posição da lista usando o método insert().
+
+numeros.insert(1, 20)
+
+# Remova o número 10 da lista usando o método remove().
+
+numeros.remove(10)
+
+# Ordene a lista em ordem crescente usando o método sort() e exiba o resultado.
+
+numeros.sort()
+
+print(numeros)
